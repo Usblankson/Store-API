d480104cd68804c2e6df70d7ef9f46876a2f13d8
Usblankson <uyohoiniblankson11@gmail.com>
2022-01-04 19:21:11 +0100

    updated the product controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,10 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({})
-    res.status(200).json({ products })
+    const products = await Product.find({
+        //name: 'name',
+    })
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
@@ -14,3 +16,4 @@ module.exports = {
     getAllProductsStatic,
 }
 
+ 
