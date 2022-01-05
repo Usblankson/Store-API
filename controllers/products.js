6275f4d2a5c57218822198f40e4c3a48937e69bd
Usblankson <uyohoiniblankson11@gmail.com>
2022-01-05 15:22:15 +0100

    updated the product controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,35 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
+    const search = 'c'
     const products = await Product.find({
-        //name: 'name',
+        name: { $regex:search, $options:'i'},
     })
     res.status(200).json({ products, nbHits: products.length })
-}
+} 
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products routes'})
+    const { featured, company, name} = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true'? true : false;
+    }
+
+    if(company){
+        queryObject.company = company;
+    }
+
+    if(name){
+        queryObject.name = { $regex:name, $options:'i'};
+    }
+    const products = await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
     getAllProducts,
     getAllProductsStatic,
-}
+} 
 
  
