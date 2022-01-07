cad27b851e8962c600d0cf0acce9ea37b1cc1a25
Usblankson <uyohoiniblankson11@gmail.com>
2022-01-07 14:08:15 +0100

    worked on product controller

--- controllers/products.js
+++ controllers/products.js
@@ -29,10 +29,7 @@ const getAllProducts = async (req, res) => {
       '<=': '$lte',
     };
     const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-    let filters = numericFilters.replace(
-      regEx,
-      (match) => `-${operatorMap[match]}-`
-    );
+    let filters = numericFilters.replace( regEx, (match) => `-${operatorMap[match]}-`);
     const options = ['price', 'rating'];
     filters = filters.split(',').forEach((item) => {
       const [field, operator, value] = item.split('-');
